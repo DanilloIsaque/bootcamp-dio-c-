3a984d17e0aa5eddd522f6ea48e8a833582538e5
DanilloIsaque <danilloisaq@gmail.com>
2024-11-23 01:06:52 -0300

    exceções e coleções

--- ExemploPropConstMet/Program.cs
+++ ExemploPropConstMet/Program.cs
@@ -42,3 +42,104 @@ DateTime data = DateTime.Now;
 Console.WriteLine(data.ToString("dd/MM/yyyy"));
 Console.WriteLine(data.ToShortDateString());
 Console.WriteLine(data.ToShortTimeString());
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("Lendo arquivo");
+
+try{
+
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+    foreach(string linha in linhas){
+        Console.WriteLine(linha);
+    }
+
+//}catch(Exception e){
+  //  Console.WriteLine($"Ocorreu uma exceção genérica.{e.Message}"); // pegamos apenas o problema e não exibimos todas os detalhes de quando dispara algum erro
+}catch (FileNotFoundException ex){
+     Console.WriteLine($"Ocorreu um erro na leitura do arquivo.{ex.Message}");
+}catch(DirectoryNotFoundException ex ){
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado {ex.Message}");
+}finally{ // serve para executar um bloco de código mesmo que ocorra uma execução ou não
+    Console.WriteLine("Finally");
+    //útil para fechar conexão com o banco,por exemplo
+}
+
+
+Console.WriteLine("Ele executa mesmo depois da exceção ser disparada");
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("Queue");
+
+Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach (int item in fila){
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"removendo o elemento :{fila.Dequeue()}"); // não passa nada, ele sempre vai remover o primeiro elemento
+
+foreach (int item in fila){
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("Stack/Pilha"); // obedece a ordem LIFO (Last in, First Out) , o último que entrou , será o primeiro a sair
+
+Stack<int> pilha = new Stack<int>();
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach (int item in pilha){
+    Console.WriteLine(item); // mostra na ordem contrária, pelo último adicionado até o primeiro
+}
+
+ // remove e retorna o objeto no topo da pilha, no caso o 10
+Console.WriteLine($"Removendo o elemento do topo : {pilha.Pop()}");
+
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("Dictionary");
+
+Dictionary<string,string> estados = new Dictionary<string,string>();
+estados.Add("SP","São Paulo");
+estados.Add("MG","Minas Gerais");
+estados.Add("RS","Rio Grande do Sul");
+estados.Add("RJ","Rio de Janeiro");
+estados.Add("BA","Bahia");
+
+foreach(KeyValuePair<string,string>  item in estados ){ //
+    Console.WriteLine($"Chave: {item.Key},  Valor: {item.Value}");
+}
+
+estados.Remove("RJ");
+// alterar valor
+estados["SP"] = "São Paulo - valor alterado";
+
+foreach(var item in estados ){ //var
+    Console.WriteLine($"Chave: {item.Key},  Valor: {item.Value}");
+}
+
+//verificar se existe chave
+string chave = "BA";
+
+if(estados.ContainsKey("BA")){
+    Console.WriteLine("Valor existente");
+}else{
+    Console.WriteLine("Não existe.");
+}
+
+//acessar o valor
+ Console.WriteLine(estados["MG"]);
