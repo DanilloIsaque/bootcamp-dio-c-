fa7b703f3e756a046d03767237acf52a5175be35
DanilloIsaque <danilloisaq@gmail.com>
2024-11-26 01:11:18 -0300

    Herança e polimorfismo

--- ExemploPOO/Models/Pessoa.cs
+++ ExemploPOO/Models/Pessoa.cs
@@ -13,7 +13,7 @@ namespace ExemploPOO.Models
         public string Nome { get; set; }
         public int Idade { get; set; }
 
-        public void Apresentar(){
+        public virtual void Apresentar(){  // colocando virtual, queremos dizer q o método pode ser sobreescrito
             Console.WriteLine($"Meu nome é {Nome} e tenho {Idade} anos"); 
         }
     }
--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploPOO.Models;
+
+Aluno a1 = new Aluno();
+a1.Nome = "Danillo";
+a1.Idade = 21;
+a1.Nota = 10;
+
+Professor p1 = new Professor();
+p1.Nome = "Isaque";
+p1.Idade = 45;
+p1.Salario = 4000;
+
+p1.Apresentar();
